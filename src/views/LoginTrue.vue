f1f7671e0ab0047c976c79095d08a3d2cf775f0e
Dion <dion.hananto14@gmail.com>
2023-05-06 23:29:21 +0700

    update 2

--- src/views/LoginTrue.vue
+++ src/views/LoginTrue.vue
@@ -69,7 +69,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .main{
     margin-bottom: 3%;
 }
@@ -79,6 +79,9 @@ export default {
     justify-content: center;
 }
 
+.main2 h4{
+    font-family: 'Inter-ExtraBold';
+}
 .whiteborder{
     background: white;
     border-radius: 3%;
@@ -109,5 +112,10 @@ export default {
 
 #details label{
     font-size: 20px;
+    font-family: 'Inter-Medium';
+}
+
+#details button{
+    font-family: 'Inter-SemiBold';
 }
 </style>
